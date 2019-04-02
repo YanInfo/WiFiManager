c3ccbb2a2254bd86c7aff703115ed416db347bb4
yaninfo163 <yaninfo@126.com>
2019-04-02 14:45:57 +0800

    first commit

---
+++ WiFiLibrary/src/main/java/com/kongqw/wifilibrary/SecurityModeEnum.java
@@ -0,0 +1,11 @@
+package com.kongqw.wifilibrary;
+
+/**
+ * Created by kongqingwei on 2017/3/8.
+ * OPEN, WEP, WPA, WPA2
+ * WIFI的四种安全性状态
+ */
+
+public enum SecurityModeEnum {
+    OPEN, WEP, WPA, WPA2
+}
